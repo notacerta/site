4e6a6195eea5c983860f422aa6da4a0805f34122
Nota Certa <contato@blognotacerta.com>
2017-12-04 00:06:49 -0300

    Commit 1.5.10: static url

--- notacertasite/settings.py
+++ notacertasite/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 
 APP_ROOT = os.path.join(BASE_DIR, 'notacertablog')
 STATIC_ROOT = os.path.join(APP_ROOT, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/notacertablog/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
