f8c7383ce792c584d2f93dd89d94ed97a419a8cd
Nota Certa <contato@blognotacerta.com>
2017-12-04 00:11:31 -0300

    Commit 1.5.11: static url

--- notacertasite/settings.py
+++ notacertasite/settings.py
@@ -6,7 +6,8 @@ Django settings for notacertasite project.
 import os
 import dj_database_url
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '3%8f7fzm8rlh!ni&$(qo0z38jp&6(xiog$$kn18c#xrsp1mbri'
 DEBUG = False
 ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
@@ -83,9 +84,19 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-APP_ROOT = os.path.join(BASE_DIR, 'notacertablog')
-STATIC_ROOT = os.path.join(APP_ROOT, 'static')
-STATIC_URL = '/notacertablog/static/'
+#APP_ROOT = os.path.join(BASE_DIR, 'notacertablog')
+#STATIC_ROOT = os.path.join(APP_ROOT, 'static')
+#STATIC_URL = '/notacertablog/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
